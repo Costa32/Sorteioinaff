5aa7ab60d125602a3a52a5bb2b8b0404400f6c73
Costa32 <84142991+Costa32@users.noreply.github.com>
2025-05-22 16:47:06 -0300

    Add social media footer to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,14 @@
 "use client";
 import { useEffect, useState } from "react";
 
-import { Flex, Spinner } from "@chakra-ui/react";
+import { Flex, Spinner, Link, Icon, Text } from "@chakra-ui/react";
+import { FaFacebook, FaInstagram, FaLinkedin, FaWhatsapp } from 'react-icons/fa';
 
 import { locationApi } from "@/services/citiesByUF";
 
 import { MainForm } from "@/components/main-form";
 
 import styles from "./page.module.css";
-
 
 export default function Home() {
   const [statesOptions, setStateOptions] = useState<
@@ -48,6 +48,42 @@ export default function Home() {
         >
           {!!loadingInfo && <Spinner />}
           {!loadingInfo && <MainForm statesOptions={statesOptions} />}
+
+          {/* Footer with social media links */}
+          <Flex 
+            direction="column" 
+            align="center" 
+            mt={8} 
+            pt={4} 
+            borderTop="1px solid #eaeaea"
+            w="100%"
+          >
+            <Link 
+              href="https://inaff.org.br" 
+              isExternal 
+              color="#517C22" 
+              fontWeight="bold" 
+              mb={4}
+            >
+              Visite inaff.org.br
+            </Link>
+            
+            <Text mb={2} color="#517C22">Siga-nos nas redes sociais:</Text>
+            <Flex gap={6}>
+              <Link href="https://facebook.com/inaff" isExternal>
+                <Icon as={FaFacebook} boxSize={6} color="#517C22" />
+              </Link>
+              <Link href="https://instagram.com/inaff" isExternal>
+                <Icon as={FaInstagram} boxSize={6} color="#517C22" />
+              </Link>
+              <Link href="https://linkedin.com/company/inaff" isExternal>
+                <Icon as={FaLinkedin} boxSize={6} color="#517C22" />
+              </Link>
+              <Link href="https://wa.me/seunumero" isExternal>
+                <Icon as={FaWhatsapp} boxSize={6} color="#517C22" />
+              </Link>
+            </Flex>
+          </Flex>
         </Flex>
       </main>
     </div>
